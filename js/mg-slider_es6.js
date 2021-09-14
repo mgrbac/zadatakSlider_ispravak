fde80d8fefe8f905c813c83483168508443cbc86
mgrbac <89588678+mgrbac@users.noreply.github.com>
2021-09-14 10:09:48 +0200

    Update mg-slider_es6.js

--- js/mg-slider_es6.js
+++ js/mg-slider_es6.js
@@ -5,152 +5,159 @@
 * 
 */
 class MGSlider {
-    constructor(options) {
-		let _this = this,
-		slider = null,
-		sliderWrapper = null,
-		slidesCurrent = null,
-		slidesOrig = null,
-		btnPrev = null,
-		btnNext = null,
-		activeSlide = null,
-		lastSlide = null,
-		firstSlide = null,
-		slideWidth = null,
-		slideMargin = null,
-		positionX = 0,
-		
-		defaultOptions = {
-			selectors: {
-				sliderId: 'slider-1',
-				btnPrevId: 'slide-prev',
-				btnNextId: 'slide-next',
-			},
-			classes: {
-				activeSlide: 'active',
-			},
-			debug: false,
-		}; 
-		
-		this.init = () => {
-			options = $.extend(true, {}, defaultOptions, options);
-			_this.setElements()
-			_this.initSlider();
-		}; 
+	slider = null;
+	sliderWrapper = null;
+	slidesCurrent = null;
+	slidesOrig = null;
+	btnPrev = null;
+	btnNext = null;
+	activeSlide = null;
+	lastSlide = null;
+	firstSlide = null;
+	slideWidth = null;
+	slideMargin = null;
+	positionX = 0;
 
-		this.calcElementWidth = (el) => parseFloat(el.outerWidth());
+	defaultOptions = {
+		selectors: {
+			sliderId: 'slider-1',
+			btnPrevId: 'slide-prev',
+			btnNextId: 'slide-next',
+		},
+		classes: {
+			activeSlide: 'active',
+		},
+		debug: false,
+	}; 
 
-		this.calcElementMargin = (el) => parseFloat(el.css('marginLeft'));
-		
-		this.getCurrentSlides = () => $('#' + options.selectors.sliderId +' .slider-wrapper .slide').length ? $('#' + options.selectors.sliderId +' .slider-wrapper .slide') : null;
-		
-		this.updateCurrentSlides = () => slidesCurrent = _this.getCurrentSlides();
-		
-		this.prependLastEl = (slides) => {
-			lastSlide = slides.last();
-			lastSlide.prependTo(sliderWrapper); 
-			
-			_this.updateCurrentSlides();
-		}; 
-
-		this.appendFirstEl = (slides) => {
-			firstSlide = slides.first();
-			firstSlide.appendTo(sliderWrapper); 
-			
-			_this.updateCurrentSlides();
-		}; 
-
-
-		this.removeActiveClass = (slide) => {
-			if (slide) {
-				slide.removeClass(options.classes.activeSlide);	
-			}
-		};     
-		
-		this.setActiveSlide = (slide) => {
-			if (slide) {
-				_this.removeActiveClass(activeSlide);
-				slide.addClass(options.classes.activeSlide);
-				activeSlide = slide;
-			}
-		}; 
-		
-		this.slidePrev = () => {
-			lastSlide = slidesCurrent.last();
-			slideWidth = _this.calcElementWidth(lastSlide);
-			slideMargin = _this.calcElementMargin(lastSlide);
-			let newPositionX = - slideWidth - slideMargin;
-			positionX = newPositionX;
-
-			sliderWrapper.animate({
-				marginRight: 0
-			  }, 500, () => {
-					sliderWrapper.css('marginRight',positionX);
-					_this.prependLastEl(slidesCurrent);
-					btnPrev.prop('disabled', false);
-			});
-			_this.setActiveSlide(activeSlide.prev());
-		}; 
-		
-		this.slideNext = () => {
-			slideWidth = _this.calcElementWidth(activeSlide);
-			slideMargin = _this.calcElementMargin(activeSlide);
-			let newPositionX = positionX - slideWidth - slideMargin;
-			positionX = - slideWidth - slideMargin;
-
-			_this.setActiveSlide(activeSlide.next());
-			
-			sliderWrapper.animate({
-				marginRight: newPositionX
-			  }, 500, () => {
-					sliderWrapper.css('marginRight',positionX);
-					_this.appendFirstEl(slidesCurrent);
-					btnNext.prop('disabled', false);
-			  });
-		}; 
-
-		this.initEvents = () => {
-			if (btnPrev) {
-				btnPrev.on('click', () => {
-					btnPrev.prop('disabled', true);
-					_this.slidePrev();
-				});
-			}
-			if (btnNext) {
-				btnNext.on('click', () => {
-					btnNext.prop('disabled', true);
-					_this.slideNext();
-				});
-			}
-		}; 
-
-		this.initSlider = () => {
-			if (!slider) {
-				_this.logCall('missing slider node: #' + options.selectors.sliderId);
-				return;
-			}
-			if (!slidesOrig) {
-				_this.logCall('missing slides');
-				return;
-			}
-
-			_this.setActiveSlide(slidesOrig.first());
-			
-			_this.prependLastEl(slidesOrig);
-			positionX = - _this.calcElementWidth(lastSlide) - _this.calcElementMargin(lastSlide);
-			sliderWrapper.css('marginRight', positionX);
-
-			_this.initEvents();
-		}; 
-
-		this.setElements = () => {
-			slider = $('#' + options.selectors.sliderId).length ? $('#' + options.selectors.sliderId) : null;
-			sliderWrapper = $('#' + options.selectors.sliderId +' .slider-wrapper').length ? $('#' + options.selectors.sliderId +' .slider-wrapper') : null;
-			slidesOrig = $('#' + options.selectors.sliderId +' .slider-wrapper .slide').length ? $('#' + options.selectors.sliderId +' .slider-wrapper .slide') : null;
-			btnPrev = $('#' + options.selectors.btnPrevId).length ? $('#' + options.selectors.btnPrevId) : null;
-			btnNext = $('#' + options.selectors.btnNextId).length ? $('#' + options.selectors.btnNextId) : null;
-		}; 
-
-		this.init();
+    	constructor(options) {
+		this.options = $.extend(true, {}, this.defaultOptions, options);
+		this.setElements();
+		this.initSlider();
 	}
+
+	calcElementWidth(el) {
+		return parseFloat(el.outerWidth());
+	}
+	
+	calcElementMargin(el) {
+		return parseFloat(el.css('marginLeft'));
+	}
+	
+	getCurrentSlides() {
+		return $('#' + this.options.selectors.sliderId +' .slider-wrapper .slide').length ? $('#' + this.options.selectors.sliderId +' .slider-wrapper .slide') : null;
+	}
+	
+	updateCurrentSlides() {
+		this.slidesCurrent = this.getCurrentSlides();
+	}
+	
+	prependLastEl(slides) {
+		this.lastSlide = slides.last();
+		this.lastSlide.prependTo(this.sliderWrapper); 
+		
+		this.updateCurrentSlides();
+	}
+
+	appendFirstEl(slides) {
+		this.firstSlide = slides.first();
+		this.firstSlide.appendTo(this.sliderWrapper); 
+		
+		this.updateCurrentSlides();
+	}
+
+	removeActiveClass(slide) {
+		if (slide) {
+			slide.removeClass(this.options.classes.activeSlide);	
+		}
+	}  
+	
+	setActiveSlide(slide) {
+		if (slide) {
+			this.removeActiveClass(this.activeSlide);
+			slide.addClass(this.options.classes.activeSlide);
+			this.activeSlide = slide;
+		}
+	}
+	
+	slidePrev() {
+		this.lastSlide = this.slidesCurrent.last();
+		this.slideWidth = this.calcElementWidth(this.lastSlide);
+		this.slideMargin = this.calcElementMargin(this.lastSlide);
+		let newPositionX = - this.slideWidth - this.slideMargin;
+		this.positionX = newPositionX;
+
+		this.sliderWrapper.animate({
+			marginRight: 0
+		  }, 500, () => {
+				this.sliderWrapper.css('marginRight', this.positionX);
+				this.prependLastEl(this.slidesCurrent);
+				this.btnPrev.prop('disabled', false);
+		});
+		this.setActiveSlide(this.activeSlide.prev());
+	}
+	
+	slideNext() {
+		this.slideWidth = this.calcElementWidth(this.activeSlide);
+		this.slideMargin = this.calcElementMargin(this.activeSlide);
+		let newPositionX = this.positionX - this.slideWidth - this.slideMargin;
+		this.positionX = - this.slideWidth - this.slideMargin;
+
+		this.setActiveSlide(this.activeSlide.next());
+		
+		this.sliderWrapper.animate({
+			marginRight: newPositionX
+		  }, 500, () => {
+				this.sliderWrapper.css('marginRight', this.positionX);
+				this.appendFirstEl(this.slidesCurrent);
+				this.btnNext.prop('disabled', false);
+		  });
+	}
+
+	initEvents(){
+		let btnPrevEl = this.btnPrev;
+		let btnNextEl = this.btnNext;
+
+		if (btnPrevEl) {
+			btnPrevEl.on('click', () => {
+				btnPrevEl.prop('disabled', true);
+				this.slidePrev();
+			});
+		}
+		
+		if (btnNextEl) {
+			btnNextEl.on('click', () => {
+				btnNextEl.prop('disabled', true);
+				this.slideNext();
+			});
+		}
+	}
+
+	initSlider() {
+		if (!this.slider) {
+			this.logCall('missing slider node: #' + this.options.selectors.sliderId);
+			return;
+		}
+		if (!this.slidesOrig) {
+			this.logCall('missing slides');
+			return;
+		}
+
+		this.setActiveSlide(this.slidesOrig.first());
+		
+		this.prependLastEl(this.slidesOrig);
+		this.positionX = - this.calcElementWidth(this.lastSlide) - this.calcElementMargin(this.lastSlide);
+		this.sliderWrapper.css('marginRight', this.positionX);
+
+		this.initEvents();
+	}
+
+	setElements() {
+		this.slider = $('#' + this.options.selectors.sliderId).length ? $('#' + this.options.selectors.sliderId) : null;
+		this.sliderWrapper = $('#' + this.options.selectors.sliderId +' .slider-wrapper').length ? $('#' + this.options.selectors.sliderId +' .slider-wrapper') : null;
+		this.slidesOrig = $('#' + this.options.selectors.sliderId +' .slider-wrapper .slide').length ? $('#' + this.options.selectors.sliderId +' .slider-wrapper .slide') : null;
+		this.btnPrev = $('#' + this.options.selectors.btnPrevId).length ? $('#' + this.options.selectors.btnPrevId) : null;
+		this.btnNext = $('#' + this.options.selectors.btnNextId).length ? $('#' + this.options.selectors.btnNextId) : null;
+	}
+
 }
